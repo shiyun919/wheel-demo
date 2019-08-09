45aeaf2466c670aca16059574854bcc0c880e913
duyu <c_duoyun@outlook.com>
2019-08-09 11:12:15 +0800

    将svg代码整理到icon.vue 实现组件化

--- src/button.vue
+++ src/button.vue
@@ -4,11 +4,7 @@
     <!--<button class="g-button" :class="{'icon-left': true}">--><!--icon在左边-->
     <!--<button class="g-button" :class="{'icon-right': true}">-->
     <!--<button class="g-button" :class="{'icon-undefined': true}">-->
-        <svg v-if="icon" class="icon" aria-hidden="true">
-            <use v-bind:xlink:href="`#i-${icon}`"></use>  <!--v-bind可以省略-->
-            <!--实际效果如下-->
-            <!--<use xlink:href="#i-settings"></use>-->
-        </svg>
+        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot> <!--slot不能添加class 所以只能添加div 给div添加class-->
         </div>
@@ -52,13 +48,14 @@
         height: var(--button-height);
         color: var(--color);
         padding: 0 12px;
+        margin-right: 4px;
         background: var(--button-background);
         border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
         display: inline-flex;
         justify-content: center;
         align-items: center;
-        vertical-align: top;
+        vertical-align: middle;
         &:hover { //&指的是.g-button
             border: 1px solid var(--boeder-color-hover);
         }
---
+++ src/icon.vue
@@ -0,0 +1,27 @@
+<template>
+    <svg class="g-icon" aria-hidden="true">
+        <use v-bind:xlink:href="`#i-${name}`"></use>  <!--v-bind可以省略-->
+        <!--实际效果如下-->
+        <!--<use xlink:href="#i-settings"></use>-->
+    </svg>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: ''
+        }
+        // ['name']
+    }
+</script>
+
+<style lang="scss">
+    .g-icon {
+        width: 1.5em;
+        height: 1.5em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+
+</style>
