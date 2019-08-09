3092f39f97647edafca11c853e675831a4e0e273
duyu <c_duoyun@outlook.com>
2019-08-09 09:11:18 +0800

    添加iconPosition属性实现icon左右位置

--- src/button.vue
+++ src/button.vue
@@ -1,18 +1,18 @@
 <!--html文件-->
 <template>
-    <button class="g-button" v-if="iconPosition === 'right'">
-        <slot></slot>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <!--<button class="g-button" :class="{'icon-left': true}">--><!--icon在左边-->
+    <!--<button class="g-button" :class="{'icon-right': true}">-->
+    <!--<button class="g-button" :class="{'icon-undefined': true}">-->
         <svg v-if="icon" class="icon" aria-hidden="true">
             <use v-bind:xlink:href="`#i-${icon}`"></use>  <!--v-bind可以省略-->
             <!--实际效果如下-->
             <!--<use xlink:href="#i-settings"></use>-->
         </svg>
-    </button>
-    <button class="g-button" v-else>    <!--v-else的取值是！iconPosition（iconPosition不存在）或iconPosition === left-->
-        <svg v-if="icon" class="icon" aria-hidden="true">
-            <use v-bind:xlink:href="`#i-${icon}`"></use>
-        </svg>
-        <slot></slot>
+        <div class="content">
+            <slot></slot> <!--slot不能添加class 所以只能添加div 给div添加class-->
+        </div>
+
     </button>
 </template>
 
@@ -30,22 +30,47 @@
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        color: var(--color);
+        padding: 0 12px;
         background: var(--button-background);
         border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
-
-        &:hover {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+        &:hover { //&指的是.g-button
             border: 1px solid var(--boeder-color-hover);
         }
-
         &:active {
             background: var(--button-active-background);
         }
-
         &:focus {
             outline: none;
         }
+        > .icon {
+            order: 1;
+            margin-right: 0.25em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin: 0 0 0 0.25em;
+            }
+            > .content {
+                order: 1;
+            }
+        }
+        //&.icon-right .icon {
+        //    order: 2;
+        //}
+        //&.icon-right .content {
+        //    order: 1;
+        //}
+
     }
 
 </style>
