f632c1ba6df5ad315600fb6f6b8647c7a0d10be9
elsa <1280752762@qq.com>
2019-09-15 11:35:29 +0800

    优化重构clo.vue

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,5 @@
 <template>
-	<div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]" 
-	:style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
+	<div class="col" :class="colClass" :style="colStyle">
 		<div class="content">
 			<slot></slot>
 		</div>
@@ -18,12 +17,27 @@
 				type: [Number, String]
 			},
 		},
-		data(){
+		data() {
 			return {
-				gutter:0
+				gutter: 0
 			}
-		}
-		
+		},
+		computed: {
+			colClass() {
+				let {
+					span,
+					offset
+				} = this // let span = this.span    let offset = this.offset
+				return [span && `col-${span}`, offset && `offset-${offset}`]
+			},
+			colStyle() {
+				return {
+					paddingLeft: this.gutter / 2 + 'px',
+					paddingRight: this.gutter / 2 + 'px'
+				}
+			}
+		},
+
 	}
 </script>
 
@@ -31,39 +45,44 @@
 	.col {
 		width: 50%;
 
-		
-		$class-prefix: col-;      //class类名前缀
-      // loops through 24 times
-      @for $n from 1 through 24 {
 
-        // for each $col_#{n} 
-        &.#{$class-prefix}#{$n} {
-			width: ($n/24) * 100%;
+		$class-prefix: col-; //class类名前缀
 
-          // set the background-color to cornflowerblue and 
-          // each time lighten the color slightly
-          //background-color: darken(cornflowerblue, 0% + ($n / 2));
-        }
-      }
-	  
-	  
-	  
-	  $class-prefix: offset-;      //class类名前缀
-	  @for $n from 1 through 24 {
-	    &.#{$class-prefix}#{$n} {
-	  			margin-left: ($n/24) * 100%;
-	    }
-	  }
-	  & .content {
-		  padding: 25px 0px;
-	  }
-	  &:nth-child(2n) > .content {
-		 background: rgb(121, 183, 255);
-	  	
-	  }
-	  &:nth-child(2n+1) > .content {
-		 background:rgb(55, 141, 241);
-	  }
-	  
+		// loops through 24 times
+		@for $n from 1 through 24 {
+
+			// for each $col_#{n} 
+			&.#{$class-prefix}#{$n} {
+				width: ($n/24) * 100%;
+
+				// set the background-color to cornflowerblue and 
+				// each time lighten the color slightly
+				//background-color: darken(cornflowerblue, 0% + ($n / 2));
+			}
+		}
+
+
+
+		$class-prefix: offset-; //class类名前缀
+
+		@for $n from 1 through 24 {
+			&.#{$class-prefix}#{$n} {
+				margin-left: ($n/24) * 100%;
+			}
+		}
+
+		& .content {
+			padding: 25px 0px;
+		}
+
+		&:nth-child(2n)>.content {
+			background: rgb(121, 183, 255);
+
+		}
+
+		&:nth-child(2n+1)>.content {
+			background: rgb(55, 141, 241);
+		}
+
 	}
 </style>
